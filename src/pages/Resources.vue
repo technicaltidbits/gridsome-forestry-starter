db3aec1b29a4204240978588a2c956a3f3dcb0bb
Deanna Thompson <deanna@techelevator.com>
2020-05-22 10:33:04 -0400

    Update resources and release notes

--- src/pages/Resources.vue
+++ src/pages/Resources.vue
@@ -1,63 +1,74 @@
 <template>
-    <Layout>
-        <div class="container">
-            <div class="resource-header">
-                <h1 class="resource-title">A tech writer's toolbox</h1>
-            </div>
-            <p>Over the last nine months or so, I've been compiling a list of resources for someone looking to transition into technical writing. Keep in mind that:</p>
-            <ol>
-                <li>This is not an exhaustive list.</li>
-                <li>You don't have to learn everything on this list.</li>
-                <li>Some of these tools and languages may not be applicable for the type of job you're seeking (i.e. not all jobs ask for candidates with DITA expertise, familiarity with JavaScript, etc).</li>
-            </ol>
-            <h2>Project management</h2>
-            <ul>
-                <li>Asana and Trello: Kanban-style project management</li>
-                <li>Confluence: notes, project planning, great for creating templates</li>
-            </ul>
-            <h2>Books</h2>
-            <ul>
-                <li>The Product is Docs: Writing technical documentation in a product development group</li>
-                <li>To the Point: A Dictionary of Concise Writing</li>
-                <li>Developing Quality Technical Information</li>
-                <li>DITA Best Practices: A Roadmap for Writing, Editing, and Architecting in DITA</li>
-                <li>Every Page is Page One</li>
-            </ul> 
-            <h2>Articles that teach you cool stuff</h2>
-            <ul>
-                <li><a href="https://www.ryanckulp.com/learn-to-code/">The Best Post Ever on Learning How to Code</a></li>
-                <li><a href="https://idratherbewriting.com/learnapidoc">Documenting APIs: A guide for technical writers and engineers</a></li>
-                <li><a href="https://contentdesign.london/content-design/user-stories-and-job-stories/">User stories and job stories</a></li>
-            </ul>
-            <h2>Software</h2>
-            <ul>
-                <li>Code editors: Visual Studio Code, Atom</li>
-                <li>Graphics: Snagit, OmniGraffle, Adobe Photoshop</li>
-                <li>Authoring tools: oXygen XML Editor, MadCap Flare, RoboHelp</li>
-                <li>Testing APIs: Postman</li>
-                <li>Testing documentation: Vale, write-good</li>
-            </ul>
-            <h2>Other tools, skills, and programming languages to know</h2>
-            <ul>
-                <li>git—<b>essential to know</b></li>
-                <li>Working with the command line</li>
-                <li>Markdown</li>
-                <li>Asciidoc</li>
-                <li>reStructured Text</li>
-                <li>HTML, CSS, and JavaScript—helpful to know the basics</li>
-                <li>DITA XML—considering learning the basics; if you find yourself interested in more and want to work in a DITA shop, then do a deep dive</li>
-                <li>Static Site Generators (SSGs)—helpful to know if you want to work in product development groups; includes Hugo, Jekyll, Gatsby, MkDocs, and Vuepress</li>
-            </ul>
-            <h3>Want to contribute to this page?</h3>
-            <p>If you know of any books, articles, or tools that new technical writers should know, I'd love to add it to this list. Reach out to me on <a href="https://www.twitter.com/itsdeannat">Twitter!</a></p>
-            <h5>last updated: <b>april 28, 2020</b></h5>
-        </div>
-        <div class="main text">
-            
-        </div>
-    </Layout>
+  <Layout>
+    <div class="container">
+      <div class="resource-header">
+        <h1 class="resource-title">A tech writer's toolbox</h1>
+      </div>
+      <p>Over the last nine months or so, I've been compiling a list of resources for someone looking to transition into the software tech writing world. It can be hard to know where to start, especially if you're transitioning into technical writing from another industry.</p>
+      <p>Don't feel pressured to learn about everything on this list; check out some of the books and articles, first. If you find yourself interested and wanting to learn more, try learning some of the software and languages mentioned below.</p>
+      <h2>Project management</h2>
+      <ul>
+        <li>Asana and Trello: Kanban-style project management</li>
+        <li>Confluence: notes, project planning, great for creating templates</li>
+      </ul>
+      <h2>Books</h2>
+      <ul>
+        <li>The Product is Docs: Writing technical documentation in a product development group</li>
+        <li>Developing Quality Technical Information</li>
+        <li>DITA Best Practices: A Roadmap for Writing, Editing, and Architecting in DITA</li>
+        <li>Every Page is Page One</li>
+      </ul>
+      <h2>Articles that teach you cool stuff</h2>
+      <ul>
+        <li>
+          <a
+            href="https://www.ryanckulp.com/learn-to-code/"
+          >The Best Post Ever on Learning How to Code</a>
+        </li>
+        <li>
+          <a
+            href="https://idratherbewriting.com/learnapidoc"
+          >Documenting APIs: A guide for technical writers and engineers</a>
+        </li>
+        <li>
+          <a
+            href="https://contentdesign.london/content-design/user-stories-and-job-stories/"
+          >User stories and job stories</a>
+        </li>
+      </ul>
+      <h2>Software</h2>
+      <ul>
+        <li>Code editors: Visual Studio Code, Atom</li>
+        <li>Graphics: Snagit, OmniGraffle, Adobe Photoshop</li>
+        <li>Authoring tools: oXygen XML Editor, MadCap Flare, RoboHelp</li>
+        <li>API development: Postman</li>
+        <li>Testing documentation: Vale, write-good linter</li>
+      </ul>
+      <h2>Other tools, skills, and languages to know</h2>
+      <ul>
+        <li>
+          git—
+          <b>essential to know</b>
+        </li>
+        <li>Working with the command line</li>
+        <li>Lightweight markup languages (Markdown, Asciidoc, reStructured Text)</li>
+        <li>The basics of frontend web development: HTML, CSS, and JavaScript</li>
+        <li>DITA XML—consider learning the basics; if you find yourself interested in more and want to work in a DITA shop, then do a deep dive</li>
+        <li>Static Site Generators (SSGs): includes Hugo, Jekyll, Gatsby, MkDocs, and Vuepress</li>
+      </ul>
+      <h3>Want to contribute to this page?</h3>
+      <p>
+        If you know of any books, articles, or tools that new technical writers should know, I'd love to add it to this list. Feel free to file a GitHub <a href="https://github.com/technicaltidbits/gridsome-forestry-starter/issues/new">issue</a> and include the resource type, a short description, and link, if applicable.
+      </p>
+      <h5>
+        last updated:
+        <b>april 28, 2020</b>
+      </h5>
+    </div>
+    <div class="main text"></div>
+  </Layout>
 </template>
 
 <script>
-export default {}
+export default {};
 </script>
